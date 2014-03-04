2b90e47d52947d92f14bc2d516f8dc2f94712652
Zach Wentz <zkwentz@gmail.com>
2014-03-03 21:08:27 -0800

    fixed navigation bar with nice easing in shadow via jquery

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 require(["jquery"], function($) {
 
     $(document).ready(function() {
-        
-        //#ffba00
-        //#f90dd5
-        
-        //1 second angle swipe yellow
-        
-        //10 seconds fade to pink
-        
-        //3 seconds swipe left to black
-        
+
+      $(window).on('scroll',function() {
+          var height = $(window).scrollTop();
+
+          console.log("Height: "+height);
+
+          if(height  > 0) {
+              $('nav').addClass('scrolled');
+          }
+          else {
+              $('nav').removeClass('scrolled');
+          }
+      });
+
     });
-    
+
 });
