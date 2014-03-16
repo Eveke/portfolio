b40a2a0d5b4850108e014d7c90de9fa4db138496
Zach Wentz <zkwentz@gmail.com>
2014-03-16 12:32:57 -0700

    added the sticking color change header to below nav when scrolled

--- js/main.js
+++ js/main.js
@@ -9,9 +9,19 @@ require(["jquery"], function($) {
 
           if(height  > 0) {
               $('nav').addClass('scrolled');
+
+              if (height >= 657)
+              {
+                $('#masthead').addClass('scrolled');
+              }
+              else
+              {
+                $('#masthead').removeClass('scrolled');
+              }
           }
           else {
               $('nav').removeClass('scrolled');
+              $('#masthead').removeClass('scrolled');
           }
       });
 
