657283cafa52b0ec1dd263c67c24050874790103
Zach Wentz <zkwentz@gmail.com>
2014-04-20 16:01:37 -0700

    server-side and client side validation

--- js/main.js
+++ js/main.js
@@ -46,15 +46,54 @@ require(["jquery"], function($) {
               method: 'post',
               success: function(data)
               {
-                $('#success').addClass('is-successful').delay(500).find('.message').fadeIn(1000);
+                $('#success').addClass('is-shown').delay(600).find('.message').fadeIn(1000);
+              },
+              error: function(jqXHR, textStatus, errorThrown)
+              {
+                $('#error').addClass('is-shown')
+                           .delay(600)
+                           .find('.message p')
+                           .html(jqXHR.responseJSON.error)
+                           .parent()
+                           .fadeIn(1000);
               }
             });
       });
 
-      $(document).on('click','#success .close',function(e){
+      $(document).on('blur', '#name', function(e){
         e.preventDefault();
-        $('#success').removeClass('is-successful');
-      })
+        var $this = $(e.target);
+        var $parent = $this.parent();
+        if ($this.val().match(/^[a-z0-9_-\s]{2,56}$/i))
+          $parent.removeClass('error').addClass('success');
+        else
+          $parent.removeClass('success').addClass('error');
+      });
+
+      $(document).on('blur', '#email', function(e){
+        e.preventDefault();
+        var $this = $(e.target);
+        var $parent = $this.parent();
+        if ($this.val().match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i))
+          $parent.removeClass('error').addClass('success');
+        else
+          $parent.removeClass('success').addClass('error');
+
+      });
+
+      $(document).on('click','.form-message .close',function(e){
+        e.preventDefault();
+        var $this = $(e.target);
+        var $form = $('#contactMe form');
+        if ($('#success').hasClass('is-shown'))
+          $form.trigger('reset');
+
+        $form.find('.error, .success').each(function(index, value){
+            $(value).removeClass('error').removeClass('success');
+        });
+
+        $('.form-message').removeClass('is-shown')
+      });
 
     });
 
